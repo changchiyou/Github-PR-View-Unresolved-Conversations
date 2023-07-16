6695d438b41a226d9b07e45b90bd95c26d9244d4
changchiyou <changchiyou@gmail.com>
2023-07-16 21:16:43 +0800

    feat: use `Tampermonkey` instead of `ShortKeys`
    because `ShortKeys` has already been deprecated for years.
    Execute the script first to generate a button instead of using shortcuts.

--- scripts/view_unresolved_conversations.js
+++ scripts/view_unresolved_conversations.js
@@ -1,82 +1,110 @@
-function scrollToElement(elementId) {
-  const element = document.getElementById(elementId);
-  if (element) {
-    const rect = element.getBoundingClientRect();
-    const scrollTop = document.documentElement.scrollTop;
-    const targetTop = rect.top + scrollTop;
+// ==UserScript==
+// @name         Github-PR-View-Unresolved-Conversations
+// @namespace    https://github.com/
+// @version      1.0
+// @description  Quickly view all Github PR unresolved-conversations
+// @match        https://github.com/*/pulls
+// @match        https://github.com/*/pull/*
+// @grant        window.onurlchange
+// ==/UserScript==
 
-    window.scrollTo({
-      top: targetTop,
-      behavior: 'smooth',
-    });
-  }
-}
+(function () {
+  "use strict";
 
-function generateConversationButtons() {
-  const unsolvedConversations = [
-    ...document.querySelectorAll('details[data-resolved="false"]')
-  ];
+  function scrollToElement(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const scrollTop = document.documentElement.scrollTop;
+      const targetTop = rect.top + scrollTop;
 
-  if (unsolvedConversations.length === 0) {
-    const noUnresolvedButton = document.getElementById('no-unresolved-button');
-    if (noUnresolvedButton) {
-      noUnresolvedButton.disabled = true;
-      noUnresolvedButton.innerHTML = 'No unresolved conversation exists';
+      window.scrollTo({
+        top: targetTop,
+        behavior: "smooth",
+      });
     }
-    return;
   }
 
-  const buttons = unsolvedConversations.map(conversation => {
-    const turboFrameId = conversation.closest('turbo-frame').id;
-    const button = document.createElement('button');
-    button.innerHTML = turboFrameId;
-    button.onclick = () => {
-      scrollToElement(turboFrameId);
-    };
-    button.className = 'btn btn-sm btn-outline';
-    button.style.marginBottom = '10px';
-    return button;
-  });
+  function generateConversationButtons() {
+    const unsolvedConversations = [
+      ...document.querySelectorAll('details[data-resolved="false"]'),
+    ];
 
-  const sortedButtons = buttons.sort((a, b) => {
-    return a.innerHTML.localeCompare(b.innerHTML);
-  });
+    if (unsolvedConversations.length === 0) {
+      const noUnresolvedButton = document.getElementById(
+        "no-unresolved-button",
+      );
+      if (noUnresolvedButton) {
+        noUnresolvedButton.disabled = true;
+        noUnresolvedButton.innerHTML = "No unresolved conversation exists";
+      }
+      return;
+    }
 
-  const reversedButtons = sortedButtons.reverse();
-
-  const discussionBucket = document.getElementById('discussion_bucket');
-  if (discussionBucket) {
-    const existingButtons = discussionBucket.querySelectorAll('.unsolved-button');
-    existingButtons.forEach(button => {
-      button.remove();
+    const buttons = unsolvedConversations.map((conversation) => {
+      const turboFrameId = conversation.closest("turbo-frame").id;
+      const button = document.createElement("button");
+      button.innerHTML = turboFrameId;
+      button.onclick = () => {
+        scrollToElement(turboFrameId);
+      };
+      button.className = "btn btn-sm btn-outline";
+      button.style.marginBottom = "10px";
+      return button;
     });
-    reversedButtons.forEach(button => {
-      discussionBucket.insertBefore(button, discussionBucket.firstChild);
+
+    const sortedButtons = buttons.sort((a, b) => {
+      return a.innerHTML.localeCompare(b.innerHTML);
+    });
+
+    const reversedButtons = sortedButtons.reverse();
+
+    const discussionBucket = document.getElementById("discussion_bucket");
+    if (discussionBucket) {
+      const existingButtons =
+        discussionBucket.querySelectorAll(".unsolved-button");
+      existingButtons.forEach((button) => {
+        button.remove();
+      });
+      reversedButtons.forEach((button) => {
+        discussionBucket.insertBefore(button, discussionBucket.firstChild);
+      });
+    }
+  }
+
+  function onPullRequestConversation() {
+    const pathnameParts = window.location.pathname.split("/");
+    const onPR = pathnameParts[3] === "pull";
+    const selectedTab = document.querySelector("a.tabnav-tab.selected");
+    const onConvTab = selectedTab && selectedTab.text.includes("Conversation");
+    return onPR && onConvTab;
+  }
+
+  function executeScript() {
+    if (onPullRequestConversation()) {
+      const discussionBucket = document.getElementById("discussion_bucket");
+      const existingButton = document.getElementById("no-unresolved-button");
+      if (!existingButton && discussionBucket) {
+        const unsolvedButton = document.createElement("button");
+        unsolvedButton.innerHTML = "Unresolved Conversations";
+        unsolvedButton.id = "no-unresolved-button";
+        unsolvedButton.onclick = () => {
+          generateConversationButtons();
+        };
+        unsolvedButton.className = "btn btn-sm btn-outline unsolved-button";
+        unsolvedButton.style.marginBottom = "10px";
+        discussionBucket.insertBefore(
+          unsolvedButton,
+          discussionBucket.firstChild,
+        );
+        console.log("'Unresolved Conversations' button has been generated.");
+      }
+    }
+  }
+
+  if (window.onurlchange === null) {
+    window.addEventListener("urlchange", () => {
+      executeScript();
     });
   }
-}
-
-function onPullRequestConversation() {
-  const pathnameParts = window.location.pathname.split('/');
-  const onPR = pathnameParts[3] === 'pull';
-  const selectedTab = document.querySelector('a.tabnav-tab.selected');
-  const onConvTab = selectedTab && selectedTab.text.includes('Conversation');
-  return onPR && onConvTab;
-}
-
-if (onPullRequestConversation()) {
-  const discussionBucket = document.getElementById('discussion_bucket');
-  if (discussionBucket) {
-    const unsolvedButton = document.createElement('button');
-    unsolvedButton.innerHTML = 'Unresolved Conversations';
-    unsolvedButton.id = 'no-unresolved-button';
-    unsolvedButton.onclick = () => {
-      generateConversationButtons();
-      unsolvedButton.disabled = true;
-      unsolvedButton.innerHTML = 'No unresolved conversation exists';
-    };
-    unsolvedButton.className = 'btn btn-sm btn-outline unsolved-button';
-    unsolvedButton.style.marginBottom = '10px';
-    discussionBucket.insertBefore(unsolvedButton, discussionBucket.firstChild);
-  }
-}
+})();
